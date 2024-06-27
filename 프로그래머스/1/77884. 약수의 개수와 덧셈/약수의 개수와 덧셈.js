d210e8f971db170ca563e842006059ed8c82b595
박영수 <160477257+youngsupark1@users.noreply.github.com>
2024-06-27 09:28:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,6 +1,8 @@
+// 제곱근이 정수면 약수의 개수가 홀수다..
 function solution(left, right) {
     var answer = 0;
     for (let i = left; i <= right; i++) {
+      	// 제곱근이 정수인지 판단
         if (Number.isInteger(Math.sqrt(i))) {
             answer -= i;
         } else {
